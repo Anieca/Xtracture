68f71949c7a83c52aeced6fb030d6a188cc4ce21
ryo.ishii <ryoishii1101@gmail.com>
2023-03-21 23:14:31 +0900

    add example

---
+++ example.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from xtracture.extractor import GPTTextExtractor, TwoStageImageExtractor
+from xtracture.recognizer import GoogleVisionRecognizer
+from xtracture.schema import ExtractTarget
+
+
+def main():
+    image_recognizer = GoogleVisionRecognizer()
+    text_extractor = GPTTextExtractor()
+    image_extractor = TwoStageImageExtractor(image_recognizer, text_extractor)
+
+    file_path = Path("./1474.jpg")
+    results = image_extractor.extract(
+        file_path,
+        [
+            ExtractTarget(key="会社名", description="落札した会社の名前"),
+            ExtractTarget(key="代表者名", description="落札した代表者の名前"),
+        ],
+    )
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
